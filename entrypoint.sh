fd501ebf06a2a2ac625e1d7553e508a1d45fc749
Bradley W Dutton <brad@duttonbros.com>
2020-11-13 20:38:36 -0800

    add some domain and protocol config vars for running behind proxies

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 # can't be done during container build or we'll end up with hardcoded values
+
+sed -i 's/date\.timezone.*//' /etc/php/7.4/cli/php.ini /etc/php/7.4/fpm/php.ini
 echo "date.timezone = ${TZ}" >> /etc/php/7.4/cli/php.ini
 echo "date.timezone = ${TZ}" >> /etc/php/7.4/fpm/php.ini
+
+sed -i 's/env\[SITE_DOMAIN\].*//' /etc/php/7.4/fpm/pool.d/www.conf
+if [ ! -z "${SITE_DOMAIN}" ]; then
+  echo "env[SITE_DOMAIN] = ${SITE_DOMAIN}" >> /etc/php/7.4/fpm/pool.d/www.conf
+fi
+
+sed -i 's/env\[SITE_PROTOCOL\].*//' /etc/php/7.4/fpm/pool.d/www.conf
+if [ ! -z "${SITE_PROTOCOL}" ]; then
+  echo "env[SITE_PROTOCOL] = ${SITE_PROTOCOL}" >> /etc/php/7.4/fpm/pool.d/www.conf
+fi
 
 chown www-data:www-data /var/www/var
 
