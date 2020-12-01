58305b5209241d3b47f66f9ad50831a2a5939d06
Bradley W Dutton <brad@duttonbros.com>
2020-11-30 20:04:23 -0800

    specify extra config options although I don't think they are doing much

--- local.php
+++ local.php
@@ -1,4 +1,5 @@
 <?php defined("SYSPATH") or die("No direct script access");
 error_reporting(E_ALL);
 ini_set('display_errors', true);
+ini_set('log_errors', true);
 ini_set('opcache.validate_timestamps', 1);
